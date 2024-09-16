f78f73869bb10250541448fa5cbad735f9dfc35b
Nicolas Herrera <herreran903@gmail.com>
2024-09-15 23:41:46 -0500

    Init commit

---
+++ src/main/java/com/api_transaction/transaction/ApiTransactionApplication.java
@@ -0,0 +1,14 @@
+package com.api_transaction.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ApiTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransactionApplication.class, args);
+	}
+}
